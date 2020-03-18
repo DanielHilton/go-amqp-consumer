6986ac1df706209477e0c2de42369ccb1f035dc8
Daniel Hilton <Daniel.hilton@bglgroup.co.uk>
2020-03-18 21:27:20 +0000

    Added working queue and exchange bindings

--- main.go
+++ main.go
@@ -8,15 +8,38 @@ import (
 )
 
 func main() {
+	qname := "go-stuff"
+	xname := "test"
+
 	uri := "amqp://guest:guest@localhost:5672/test"
 	conn, err := amqp.Dial(uri)
 	failOnError(err, "Failed to open a connection")
-	fmt.Printf("Connected to %s successfully", uri)
+	fmt.Printf("Connected to %s successfully\n", uri)
 	defer conn.Close()
 
 	channel, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer channel.Close()
+
+	err = channel.ExchangeDeclare(
+		xname, "fanout",
+		true, false, false, false, nil)
+	failOnError(err, "Failed to declare exchange")
+	fmt.Printf("Exchange %s declared\n", xname)
+
+	_, err = channel.QueueDeclare(
+		qname, // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to create queue")
+	fmt.Printf("Queue %s declared\n", qname)
+
+	err = channel.QueueBind("go-stuff", "test.go-stuff", "test", false, nil)
+	failOnError(err, "Failed to bind queue to exchange")
 }
 
 func failOnError(err error, msg string) {
